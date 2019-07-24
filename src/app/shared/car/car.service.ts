f38da5d9bf04a33973e8dfe1278702d6c628aaaf
lllll <dovanliem09102014@gmail.com>
2019-07-23 21:01:51 -1000

    Sping boot CRUD + angular 7

---
+++ src/app/shared/car/car.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+  private API = 'http://localhost:8080/';
+  private CAR_API = this.API + '/cars';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<any> {
+    return this.http.get('//localhost:8080/cool-cars');
+  }
+
+  get(id: string) {
+    return this.http.get(this.CAR_API + '/' + id);
+  }
+
+  save(car: any): Observable<any> {
+    let result: Observable<Object>;
+    if (car['href']) {
+      result = this.http.put(car.href, car);
+    } else {
+      result = this.http.post(this.CAR_API, car);
+    }
+    return result;
+  }
+
+  remove(href: string) {
+    return this.http.delete(href);
+  }
+}
